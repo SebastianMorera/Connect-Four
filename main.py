b54b9452768617675259618b5e3a37b20804fccc
Sebastian Morera <sebas.morera999@hotmail.com>
2022-03-21 22:28:03 -0400

    Game implemented with graphics

--- main.py
+++ main.py
@@ -1,2 +1,152 @@
+import pygame
+import numpy as np
+from scipy.signal import convolve2d
+import sys
+import math
+
+CYAN_BLUE = (0, 255, 255)
+BLACK = (0, 0, 0)
+RED = (245, 84, 66)
+YELLOW = (200, 245, 66)
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+SQUARE_SIZE = 100
+WIDTH = COLUMN_COUNT * SQUARE_SIZE
+HEIGHT = (ROW_COUNT + 1) * SQUARE_SIZE
+size = (WIDTH, HEIGHT)
+RADIUS = int(SQUARE_SIZE / 2 - 5)
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Connect four')
+wood = pygame.image.load("Graphics/wood.jpg")
+myFont = pygame.font.SysFont("monospace", 75)
+
+# Kernel: In mathematics, the kernel of a linear map, also known as the null space or nullspace, is the linear subspace
+# of the domain of the map which is mapped to the zero vector.
+horizontal_kernel = np.array([[1, 1, 1, 1]])
+vertical_kernel = np.transpose(horizontal_kernel)
+diag1_kernel = np.eye(4, dtype=np.uint8)
+diag2_kernel = np.fliplr(diag1_kernel)
+detection_kernels = [horizontal_kernel, vertical_kernel, diag1_kernel, diag2_kernel]
+
+
+def create_board():
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+    return board
+
+
+def drop_piece(board, row, col, piece):
+    board[row, col] = piece
+
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT - 1][col] == 0
+
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+
+def winning_move(board, player):
+    for kernel in detection_kernels:
+        if (convolve2d(board == player, kernel, mode="valid") == 4).any():
+            return True
+    return False
+
+
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            # pygame.draw.rect(screen, CYAN_BLUE,
+            #                  (c * SQUARE_SIZE, r * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            screen.blit(wood, (c * SQUARE_SIZE, r * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            pygame.draw.circle(screen, BLACK, (
+                int(c * SQUARE_SIZE + SQUARE_SIZE / 2), int(r * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE / 2)),
+                               RADIUS)
+
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (
+                    int(c * SQUARE_SIZE + SQUARE_SIZE / 2), HEIGHT - int(r * SQUARE_SIZE + SQUARE_SIZE / 2)),
+                                   RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (
+                    int(c * SQUARE_SIZE + SQUARE_SIZE / 2), HEIGHT - int(r * SQUARE_SIZE + SQUARE_SIZE / 2)),
+                                   RADIUS)
+
+    pygame.display.update()
+
+
+def ConnectFour():
+    board = create_board()
+    draw_board(board)
+    game_over = False
+    turn = 0
+
+    while not game_over:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
+                posx = event.pos[0]
+                if turn == 0:
+                    pygame.draw.circle(screen, RED, (posx, int(SQUARE_SIZE / 2)), RADIUS)
+                else:
+                    pygame.draw.circle(screen, YELLOW, (posx, int(SQUARE_SIZE / 2)), RADIUS)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Ask for Player 1 Input
+                if turn == 0:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx / SQUARE_SIZE))
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 1)
+
+                        if winning_move(board, 1):
+                            pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
+                            label = myFont.render("Player 1 wins!!", 1, RED)
+                            screen.blit(label, (40, 10))
+                            game_over = True
+
+                # Ask for Player 2 Input
+                else:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx / SQUARE_SIZE))
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 2)
+
+                    if winning_move(board, 2):
+                        pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, SQUARE_SIZE))
+                        label = myFont.render("Player 2 wins!!", 1, YELLOW)
+                        screen.blit(label, (40, 10))
+                        game_over = True
+
+                turn += 1
+                turn = turn % 2
+
+        draw_board(board)
+
+    pygame.time.wait(3000)
+
+
 if __name__ == "__main__":
-    print("Connect four game implemented with Pygame module!")
+    print("Connect four game implemented with Pygame module! \n")
+
+    ConnectFour()
